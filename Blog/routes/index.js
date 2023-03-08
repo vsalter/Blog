96482eabc20aa9e1a567ffa80df2bd34049a51b4
vsalter <vsalter1@student.gsu.edu>
2023-03-08 09:30:51 -0500

    added about, contact, and post pages. Updated index router. Updated nav and image links

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Blog' });
+  res.render('index', { title: 'Blog - Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Blog - About' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Blog - Contact' });
+});
+
+router.get('/post', function(req, res, next) {
+  res.render('post', { title: 'Blog - Post' });
 });
 
 module.exports = router;
